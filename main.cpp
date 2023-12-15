814a97312e0dc41efc777179680ed7e1c8933c5f
raphaelprados <santosapraphael@gmail.com>
2023-12-14 22:44:19 -0400

    codigo escrito, precisa de testes

--- main.cpp
+++ main.cpp
@@ -3,29 +3,52 @@
 #include <process.h>
 #include <random>
 #include <time.h>
-#include <queue>
-    
+#include <vector>    
+#include <algorithm>
+#include <process.h>
+#include <windows.h>
+#include <thread>
+#include <chrono>
+
+// Estruturas de dados
+struct filosofo {
+    int id;
+    double t_meditacao;
+    char estado;            // c = comendo, d = dormindo, e = esperando
+};
+
 // Variáveis Globais
-double t_meditacao_filosofos[5];
+std::vector<struct filosofo> filosofos = { {0, 0.0, 'd'}, {1, 0.0, 'd'}, {2, 0.0, 'd'}, {3, 0.0, 'd'}, {4, 0.0, 'd'} };
 double T_BASE = 0.05;
 //std::queue<int> fila;
-int fila[5] = {-1, -1, -1, -1, -1};
+std::vector<int> fila;
+bool garfos[5] = {false, false, false, false, false};
 int NFILS = 5;
 int acordados = 0;
-    
+int NITS = 5;
+
+// Mutex
+HANDLE mutex_garfo;
+HANDLE mutex_fila;
+
 // Declaração de funções
+int verificar_prioridade(int, int);
 void tempo_meditacao();
-void dormir();
-void acordar();
-void pegar_dir();
-void pegar_esq();
-void comer();
+void dormir(int);
+void acordar(int);
+void comer(int);
+void func_thread(void *);
 
 int main() {
 
+    // Vetor de Threads
+    std::vector<HANDLE> hThreads;
+    // Cria as threads
+    for(int i = 0; i < NFILS; i++)
+        hThreads.push_back(CreateThread(NULL, 0, (LPTHREAD_START_ROUTINE)&func_thread, &filosofos[i], 0, NULL));
+
     // Pre-processamento
     tempo_meditacao();
-    
 
     return 0;
 }
@@ -35,31 +58,63 @@ int main() {
 // Origem da expressão: https://stackoverflow.com/questions/686353/random-float-number-generation
 void tempo_meditacao() {
     for (int i = 0; i < NFILS; i++) {
-        t_meditacao_filosofos[i] = T_BASE/2 + static_cast <float> (rand()) / static_cast <float> (RAND_MAX / T_BASE);
-        //std::cout << "t" << i << " = " << t_meditacao_filosofos[i] << std::endl;
+        filosofos[i].t_meditacao = T_BASE / 2 + static_cast <float> (rand()) / static_cast <float> (RAND_MAX / T_BASE);
+        std::cout << "t" << i << " = " << filosofos[i].t_meditacao << std::endl;
     }
 }
 
-void acordar() {
-
+void acordar(int id) {
+    fila.push_back(id);
+    filosofos[id].estado = 'e';
 }
 
-void dormir() {
-
+void dormir(int id) {
+    // remove o filosofo que acabou de comer e irá dormir
+    filosofos[id].estado = 'd';
+    for (int i = 0; i < fila.size(); i++)
+        if (fila[i] == id)
+            fila.erase(std::remove(fila.begin(), fila.end(), id), fila.end());
+    // Fonte: https://stackoverflow.com/questions/3385229/c-erase-vector-element-by-value-rather-than-by-position
+    
+    // Chamada para thread/filosofo dormir
+    std::this_thread::sleep_for(std::chrono::duration<double>(filosofos[id].t_meditacao));
 }
 
-void pegar_dir() {
-
+void comer(int id) {
+    filosofos[id].estado = 'e';
+    while (!(!garfos[(id + 1) % 5] && !garfos[id] && (verificar_prioridade(id, id - 1) == id) && (verificar_prioridade(id, id + 1)))) {
+        std::this_thread::sleep_for(std::chrono::duration<double>(T_BASE));
+    }
+    filosofos[id].estado = 'c';
+    
+    WaitForSingleObject(mutex_garfo, INFINITE);
+        garfos[(id + 1) % 5] = true;
+        garfos[id] = true;
+    ReleaseMutex(mutex_garfo);
+    std::this_thread::sleep_for(std::chrono::duration<double>(T_BASE));
+    WaitForSingleObject(mutex_garfo, INFINITE);
+        garfos[(id + 1) % 5] = false;
+        garfos[id] = false;
+    ReleaseMutex(mutex_garfo);
 }
 
-void pegar_esq() {
-
+int verificar_prioridade(int id_a, int id_b) {
+    // retorna o item que aparece primeiro no vetor
+    for(int i = 0; i < fila.size(); i++) {
+        if (fila[i] == id_a)
+            return id_a;
+        else if (fila[i] == id_b)
+            return id_b;
+    }
+    return -1;
 }
 
-void comer() {
-
+void func_thread(void *parametros) {
+    struct filosofo* fil = (struct filosofo*)parametros;
+    // Roda por NITS iterações
+    for (int i = 0; i < NITS; i++) {
+        dormir(fil->id);
+        comer(fil->id);
+    }
 }
 
-void atualiza_fila() {
-
-}
